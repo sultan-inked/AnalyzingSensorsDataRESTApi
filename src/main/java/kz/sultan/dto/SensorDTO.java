86c1e805dae444e23006c593328cfabf87fa84a5
Sultan <nikowosuka@gmail.com>
2024-06-09 12:13:46 +0500

    ADD: dto.SensorDTO.java

---
+++ src/main/java/kz/sultan/dto/SensorDTO.java
@@ -0,0 +1,21 @@
+package kz.sultan.dto;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Size;
+
+public class SensorDTO {
+	@Column(name = "name")
+	@NotEmpty(message = "Name should not be empty")
+	@Size(min = 1, max = 100, message = "Name should be between 1 and 100 characters")
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
