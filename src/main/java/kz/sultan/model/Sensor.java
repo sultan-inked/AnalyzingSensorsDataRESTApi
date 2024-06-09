37c4bf7d19f26f02d5188eec289d4c7b914494ef
Sultan <nikowosuka@gmail.com>
2024-06-09 12:10:40 +0500

    ADD: model.Sensor.java

---
+++ src/main/java/kz/sultan/model/Sensor.java
@@ -0,0 +1,70 @@
+package kz.sultan.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "Sensor")
+public class Sensor {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "name")
+	@NotEmpty(message = "Name should not be empty")
+	@Size(min = 1, max = 100, message = "Name should be between 1 and 100 characters")
+	private String name;
+	
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+	
+	public Sensor(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
