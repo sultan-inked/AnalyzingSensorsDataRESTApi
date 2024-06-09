8198aca086da5d26146cd05a6f0e50952bec567c
Sultan <nikowosuka@gmail.com>
2024-06-09 12:17:15 +0500

    ADD: controllers.SensorsController.java

---
+++ src/main/java/kz/sultan/controllers/SensorsController.java
@@ -0,0 +1,11 @@
+package kz.sultan.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sensors")
+public class SensorsController {
+	
+	
+}
