ab995692b6dc8ffc626d9cc5e1d8cfee36542063
Sultan <nikowosuka@gmail.com>
2024-06-09 12:30:06 +0500

    ADD: services.SensorsService.java

---
+++ src/main/java/kz/sultan/services/SensorsService.java
@@ -0,0 +1,41 @@
+package kz.sultan.services;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kz.sultan.model.Sensor;
+import kz.sultan.repositories.SensorsRepository;
+
+@Service
+@Transactional(readOnly = true)
+public class SensorsService {
+	
+	private final SensorsRepository sensorsRepository;
+	
+	@Autowired
+	public SensorsService(SensorsRepository sensorsRepository) {
+		this.sensorsRepository = sensorsRepository;
+	}
+	
+	public List<Sensor> findAll() { return sensorsRepository.findAll(); }
+	
+	public Sensor findOne(int id) {
+		Optional<Sensor> foundSensor = sensorsRepository.findById(id);
+		return foundSensor.orElse(null);
+	}
+	
+	@Transactional
+	public void save(Sensor sensor) {
+		enrichSensor(sensor);
+		sensorsRepository.save(sensor);
+	}
+	private void enrichSensor(Sensor sensor) {
+		sensor.setCreatedAt(LocalDateTime.now());
+		sensor.setUpdatedAt(LocalDateTime.now());
+	}
+}
