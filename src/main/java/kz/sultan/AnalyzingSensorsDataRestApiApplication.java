6d1f45ca63d70a39f41eeb11009d901dd655577a
Sultan <nikowosuka@gmail.com>
2024-06-09 11:12:47 +0500

    First commit

---
+++ src/main/java/kz/sultan/AnalyzingSensorsDataRestApiApplication.java
@@ -0,0 +1,13 @@
+package kz.sultan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzingSensorsDataRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzingSensorsDataRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/sultan/AnalyzingSensorsDataRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sultan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzingSensorsDataRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
